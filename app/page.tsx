48393d394a323d02a1a49cdf48024a2d167bd5a0
Vashishta Mithra Reddy Vundyala <vashishtamithrareddy@gmail.com>
2025-10-26 12:06:50 +0530

    clean up

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,14 @@ import BottomGradient from "@/components/BottomGradient";
 export default function Home() {
   return (
     <>
-    <LoadingAnimation>
+    {/* <LoadingAnimation> */}
     {/* <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-satoshi animate-in fade-in">
       <h1 className="text-3xl font-bold">
         Vashishta Mithra Reddy | V19
       </h1>
     </div> */}
     <Projects/>
-    </LoadingAnimation>
+    {/* </LoadingAnimation> */}
     </>
   );
 }
